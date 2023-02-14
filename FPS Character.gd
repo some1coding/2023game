01ec4b1d71b02160184eee501d846d9f47d83baa
some1coding <some.dude.at.someplace@gmail.com>
2023-02-15 12:25:57 +1300

    add ability to look
    you can look around (with limits to how far you can turn your head)

---
+++ FPS Character.gd
@@ -0,0 +1,14 @@
+extends CharacterBody3D
+
+@onready var head = $Head
+
+var mouse_sensitivity = 0.05
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
+		head.rotate_x(deg_to_rad(-event.relative.y * mouse_sensitivity))
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
