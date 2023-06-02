8b3f5080d4345743a017dd35c6d075ad65cb0308
some1coding <some.dude.at.someplace@gmail.com>
2023-06-02 15:00:19 +1200

    Add Main Menu
    added main menu

---
+++ Menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _on_play_pressed():
+	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
+
+func _on_options_pressed():
+	pass # Replace with function body.
+	
+
+func _on_exit_pressed():
+	pass # Replace with function body.
+	get_tree().quit()
